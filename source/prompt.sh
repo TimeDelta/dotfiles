d0372deba22c3b4f8384e4f5e3a33caee68c7715
Bryan Herman <EMAIL>
2015-02-04 18:19:13 -0500

    modified prompt so that resourcing the prompt file doesn't include multiple copies of the custom prompt code in the PROMPT_COMMAND var

--- source/prompt.sh
+++ source/prompt.sh
@@ -1,1 +1,3 @@
-PROMPT_COMMAND="export PS1=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FCYAN}\`svnbranch\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";$PROMPT_COMMAND"
+prompt="export PS1=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FBLUE}\`svnbranch\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
+ # make sure there's only one copy of the prompt code
+export PROMPT_COMMAND="${prompt}`echo $PROMPT_COMMAND | sed 's|$prompt||'`"
