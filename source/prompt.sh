35015fe6b94a3b58c6ba39afb92810a0a527cc79
Bryan Herman <EMAIL>
2015-02-06 16:06:06 -0500

    fixed conflict between custom PS1 prompt and iterm2's shell integration script

--- source/prompt.sh
+++ source/prompt.sh
@@ -1,3 +1,4 @@
-prompt="export PS1=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FBLUE}\`svnbranch\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
+is_osx && ps1_var=orig_ps1 || ps1_var=PS1
+prompt="export $ps1_var=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FBLUE}\`svnbranch\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
  # make sure there's only one copy of the prompt code
 export PROMPT_COMMAND="${prompt}`echo $PROMPT_COMMAND | sed 's|$prompt||'`"
