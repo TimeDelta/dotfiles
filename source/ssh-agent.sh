14eeaa0626aee5d286f575968589012ed19d9fcc
Ben Alman <EMAIL>
2015-02-27 17:39:51 -0500

    fixed bug where a new instance of ssh-agent would be spawned every time source files were sourced

--- source/ssh-agent.sh
+++ source/ssh-agent.sh
@@ -1,3 +1,3 @@
 # this makes GitHub accept push commands
-eval "$(ssh-agent -s)"
+ps -xc -o command | grep ssh-agent || eval "$(ssh-agent -s)" # only need one instance of ssh-agent
 ssh-add ~/.ssh/id_github
