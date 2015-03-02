995376a6efef8c6dfd50cae9612cd19c1eff1647
Bryan Herman <EMAIL>
2015-03-02 11:34:00 -0500

    improved ssh-agent solution

--- source/ssh-agent.sh
+++ source/ssh-agent.sh
@@ -1,3 +1,4 @@
 # this makes GitHub accept push commands
-[[ -n `lsof -tc ssh-agent` ]] || eval "$(ssh-agent -s)" # only need one instance of ssh-agent
-ssh-add ~/.ssh/id_github
+f="$HOME/.ssh-agent-output"
+[[ -n `lsof -tc ssh-agent` ]] || { eval "$(ssh-agent -s | tee "$f")" && ssh-add ~/.ssh/id_github; } # only need one instance of ssh-agent
+[[ -n $SSH_AGENT_PID ]] || eval "$(cat "$f")" # basically this is for when a new session is opened and ssh-agent is already running
