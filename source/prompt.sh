d75385f44d842f9ad149a7621f33551ae7495303
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-03-02 10:38:09 -0500

    updated prompt to reflect change in function name for svnbranch

--- source/prompt.sh
+++ source/prompt.sh
@@ -1,6 +1,6 @@
 # this reconciles a conflict between the iterm integration script and having a custom prompt
 { is_osx && ps1_var=orig_ps1; } || { [[ `hostname` =~ cluster*|mima ]] && ps1_var=orig_ps1; } || ps1_var=PS1
 
-prompt="export $ps1_var=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FBLUE}\`svnbranch\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
+prompt="export $ps1_var=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FBLUE}\`svnb\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
  # make sure there's only one copy of the prompt code
 export PROMPT_COMMAND="${prompt}`echo $PROMPT_COMMAND | sed 's|$prompt||'`"
