fe185b238b41b15545c7be697f84787deb54d238
Ben Alman <EMAIL>
2015-03-02 10:22:52 -0500

    fixed minor bug with ssh-agent.sh

--- source/ssh-agent.sh
+++ source/ssh-agent.sh
@@ -1,4 +1,5 @@
 # this makes GitHub accept push commands
-ssh_agent_pid=`ps -xc -o command | grep ssh-agent`
+is_osx && ssh_agent_pid=`ps -xc -o command | grep ssh-agent`
+is_linux && ssh_agent_pid=`ps -A -o command | grep ssh-agent`
 [[ -n $ssh_agent_pid ]] || eval "$(ssh-agent -s)" # only need one instance of ssh-agent
 ssh-add ~/.ssh/id_github
