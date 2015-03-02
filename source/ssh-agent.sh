83300f39e639853947ac03c157cf191db6ba516f
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-03-02 09:33:13 -0500

    removed displaying of ssh-agent on login if it is already running

--- source/ssh-agent.sh
+++ source/ssh-agent.sh
@@ -1,3 +1,4 @@
 # this makes GitHub accept push commands
-ps -xc -o command | grep ssh-agent || eval "$(ssh-agent -s)" # only need one instance of ssh-agent
+ssh_agent_pid=`ps -xc -o command | grep ssh-agent`
+[[ -n $ssh_agent_pid ]] || eval "$(ssh-agent -s)" # only need one instance of ssh-agent
 ssh-add ~/.ssh/id_github
