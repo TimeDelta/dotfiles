5044428a956a695ad099b44f6ac85f7113629063
Bryan Herman <EMAIL>
2015-03-02 14:35:27 -0500

    fixed conflict between iterm integration script and custom prompt in screen session

--- source/prompt.sh
+++ source/prompt.sh
@@ -1,5 +1,7 @@
 # this reconciles a conflict between the iterm integration script and having a custom prompt
-{ is_osx && ps1_var=orig_ps1; } || { [[ `hostname` =~ cluster*|mima ]] && ps1_var=orig_ps1; } || ps1_var=PS1
+{ is_osx && [[ "$TERM" != "screen" ]] && ps1_var=orig_ps1; } || \
+{ [[ `hostname` =~ cluster*|mima ]] && [[ "$TERM" != "screen" ]] && ps1_var=orig_ps1; } || \
+ps1_var=PS1
 
 prompt="export $ps1_var=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FBLUE}\`svnb\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
  # make sure there's only one copy of the prompt code
