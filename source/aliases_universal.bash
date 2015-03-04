4af8fb5d1aa3919953606b6245fe7421318c7add
Bryan Herman <EMAIL>
2015-03-04 16:46:02 -0500

    updated HISTIGNORE

--- source/aliases_universal.bash
+++ source/aliases_universal.bash
@@ -1250,7 +1250,7 @@ export HISTSIZE=9999999999
 
 # ignore commands that start with a ' ' and duplicate commands
 export HISTCONTROL=ignoreboth
-export HISTIGNORE='sudo &:[fb]g:rs:exit:logout:ls?([la]):ls -[la]:svn[bcr]:up:ci ?(-m )"*":cluster[0-9]:?(p)aliases:s?(p)alias:sdirs:@(cd|..|?(h)top|history?(|*))?( *):pa_@(cluster|mac)'
+export HISTIGNORE='sudo &:[fb]g:rs:exit:logout:ls?([la]):ls -[la]:svn[bcr]:up:st:ci?( ?(-m )"*"):cluster[0-9]:?(p)aliases:s?(p)alias:src:sdirs:@(cd|..|?(h)top|history?(|*))?( *):pa_@(cluster|mac)'
 
 # store the time that each command was entered
 export HISTTIMEFORMAT='%F %T '
