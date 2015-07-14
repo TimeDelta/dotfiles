ddc65607c1f8f9018ed7b29601b686db713eb557
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-07-14 10:34:26 -0400

    added git branch name to prompt for directories that contain a git repository. made the ps1_var logic more legible.

--- source/prompt.sh
+++ source/prompt.sh
@@ -1,8 +1,20 @@
 # this reconciles a conflict between the iterm integration script and having a custom prompt
-{ is_osx && [[ "$TERM" != "screen" ]] && ps1_var=orig_ps1; } || \
-{ [[ `hostname` =~ cluster*|mima ]] && [[ "$TERM" != "screen" ]] && ps1_var=orig_ps1; } || \
-ps1_var=PS1
+if [[ is_osx && "$TERM" != "screen" ]]; then
+	ps1_var=orig_ps1
+elif [[ `hostname` =~ cluster*|mima && "$TERM" != "screen" ]]; then
+	ps1_var=orig_ps1
+else
+	ps1_var=PS1
+fi
 
-prompt="export $ps1_var=\"\$(if [[ -n \`svnr 2> /dev/null\` ]]; then echo \"[${FBLUE}\`svnb\`${RES}:${FCYAN}\`svnr\`${RES}]\"; fi; )${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
+prompt (){
+	if [[ -n `svnr 2> /dev/null` ]]; then
+		echo "[${FBLUE}`svnb`${RES}:${FCYAN}`svnr`${RES}]"
+	elif [[ -n `git log 2> /dev/null` ]]; then
+		echo "[${FBLUE}`git branch | egrep '^\*' | sed 's/^..//'`${RES}]"
+	fi
+}
+
+prompt="export $ps1_var=\"\$(prompt)${FGREEN}\h${RES}:${FYELLOW}${BOLD}\w${RES}\n\$ \";"
  # make sure there's only one copy of the prompt code
 export PROMPT_COMMAND="${prompt}`echo $PROMPT_COMMAND | sed 's|$prompt||'`"
