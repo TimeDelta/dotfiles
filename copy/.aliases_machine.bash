5e1e95feb030014052f8c21603808389d44d4574
Bryan Herman <EMAIL>
2015-08-27 16:26:29 -0400

    added default machine aliases file for new dotfiles installtions

---
+++ copy/.aliases_machine.bash
@@ -0,0 +1,8 @@
+################################################################################
+# Notes:
+# This file contains aliases and functions that are specific to this machine.
+# Aliases and functions followed by a "# [BH]" is written entirely by me
+# Aliases and functions followed by a "# {BH}" is adapted by me from somebody else's code
+# Aliases and functions without a comment after it is completely taken from elsewhere
+################################################################################
+
