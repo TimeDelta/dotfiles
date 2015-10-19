b27d38f8653ade0a4ce20ff43819afe402e0a73a
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-10-19 11:04:20 -0400

    made sublcp much more robust and more universal

--- source/aliases_sublime.bash
+++ source/aliases_sublime.bash
@@ -184,10 +184,6 @@ is_osx || return 1
 
 # sublcp: close project in Sublime Text
 sublcp() { # [BH]
-	# NOTE: this will only work if you add this keyboard shortcut in the system preferences panel:
-	#       cmd+alt+shift+ctrl-w
-	osascript -e '
-		tell application "Sublime Text" to activate
-		tell application "System Events" to keystroke "w" using {command down, option down, shift down, control down}
-	'
+	subl --command close_workspace
+	subl --command close_project
 }
