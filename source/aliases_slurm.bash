105f481cf18477752d92a5ed02b305b71b9b27ef
Bryan Nova <bryanherman7@gmail.com>
2026-06-12 12:17:51 -0400

    add slurmnodes alias

--- source/aliases_slurm.bash
+++ source/aliases_slurm.bash
@@ -9,4 +9,5 @@ trackGPUmem() {
 }
 alias slurmjobs="squeue --format='%5i   %50j   %10u   %10T   %9M   %R'"
 alias myslurmjobs='slurmjobs -u "`whoami`"'
+alias slurmnodes='sinfo -N -o "%.20N %.12P %.8c %.12m %.25G %.10t"'
 
